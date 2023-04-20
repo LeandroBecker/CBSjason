8a9498257636e750dbf6d0cdf035037af343fcc9
LeandroBecker <leandro.becker@ufsc.br>
2023-04-20 09:15:05 +0100

    updated LBB-ROSinterfAnalysis

--- examples/LBB-ROSinterfAnalysis/catkin_ws/devel/_setup_util.py
+++ examples/LBB-ROSinterfAnalysis/catkin_ws/devel/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/leandro/jason/examples/lbb_test_maiquel/catkin_ws/devel;/opt/ros/noetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/leandro/jason/examples/LBB-ROSinterfAnalysis/catkin_ws/devel;/opt/ros/noetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
